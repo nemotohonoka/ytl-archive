4580492ef847ced48615fcb238637590e3f50673
根本萌香 <h.nemoto@p-oh.jp>
2025-08-15 13:41:06 +0900

    header 検索ボタン追加

--- header.php
+++ header.php
@@ -28,90 +28,103 @@
         </a>
       </h1>
 
-      <div id="nav-toggle">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-
-      <nav class="sp-nav">
-        <h1>
-          <a href="<?php echo home_url(); ?>">
-            <div class="logo">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo_white.svg" alt="">
-            </div>
+      <div class="right-box">
+        <div class="search-button">
+          <a href="<?php echo home_url(); ?>/corporation/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30.253" height="33.159" viewBox="0 0 30.253 33.159" aria-hidden="true" focusable="false">
+              <g transform="translate(-1201 -22)">
+                <path d="M14,2A12,12,0,0,0,5.515,22.485,12,12,0,1,0,22.485,5.515,11.921,11.921,0,0,0,14,2m0-2A14,14,0,1,1,0,14,14,14,0,0,1,14,0Z" transform="translate(1201 22)" fill="currentColor"/>
+                <path d="M6.247,8.658l-7-8L.753-.659l7,8Z" transform="translate(1223.5 46.5)" fill="currentColor"/>
+              </g>
+            </svg>
           </a>
-        </h1>
+        </div>
 
-        <div class="inner">
-          <div class="main-wrapper">
-						<div class="main-nav">
-              <div class="menu-lavel">
-                <p>Company Information</p>
-                <ul>
-                  <li><a href="<?php echo home_url(); ?>">ホーム<span>Home</span></a></li>
-                  <li><a href="<?php echo home_url(); ?>/news/">お知らせ<span>News</span></a></li>
-                  <li><a href="<?php echo home_url(); ?>/about/">会社概要<span>About</span></a></li>
-                </ul>
+        <div id="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
 
-                <div class="sub-nav sub-nav-pc">
-                  <div class="movie-library">
-                    <a href="<?php echo home_url(); ?>/plan/"><span>動画ライブラリ</span></a>
+        <nav class="sp-nav">
+          <h1>
+            <a href="<?php echo home_url(); ?>">
+              <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo_white.svg" alt="">
+              </div>
+            </a>
+          </h1>
+
+          <div class="inner">
+            <div class="main-wrapper">
+              <div class="main-nav">
+                <div class="menu-lavel">
+                  <p>Company Information</p>
+                  <ul>
+                    <li><a href="<?php echo home_url(); ?>">ホーム<span>Home</span></a></li>
+                    <li><a href="<?php echo home_url(); ?>/news/">お知らせ<span>News</span></a></li>
+                    <li><a href="<?php echo home_url(); ?>/about/">会社概要<span>About</span></a></li>
+                  </ul>
+
+                  <div class="sub-nav sub-nav-pc">
+                    <div class="movie-library">
+                      <a href="<?php echo home_url(); ?>/plan/"><span>動画ライブラリ</span></a>
+                    </div>
+
+                    <div class="material-library">
+                      <a href="<?php echo home_url(); ?>/corporation/"><span>スライド資料</span></a>
+                    </div>
                   </div>
+                </div>
 
-                  <div class="material-library">
-                    <a href="<?php echo home_url(); ?>/corporation/"><span>スライド資料</span></a>
-                  </div>
+                <div class="menu-lavel">
+                  <p>Medical Resources</p>
+                  <ul>
+                    <li><a href="<?php echo home_url(); ?>//">疾患別コンテンツ<span>Disease-Specific</span></a></li>
+                    <li><a href="<?php echo home_url(); ?>//">医療制度コンテンツ<span>Medical System</span></a></li>
+                    <li><a href="<?php echo home_url(); ?>//">スキル研修<span>Skills Training</span></a></li>
+                    <li><a href="<?php echo home_url(); ?>//">Web講演会<span>Webinars</span></a></li>
+                    <li><a href="<?php echo home_url(); ?>//">情報提供資材<span>Webinars</span></a></li>
+                  </ul>
                 </div>
               </div>
 
-              <div class="menu-lavel">
-                <p>Medical Resources</p>
-                <ul>
-                  <li><a href="<?php echo home_url(); ?>//">疾患別コンテンツ<span>Disease-Specific</span></a></li>
-                  <li><a href="<?php echo home_url(); ?>//">医療制度コンテンツ<span>Medical System</span></a></li>
-                  <li><a href="<?php echo home_url(); ?>//">スキル研修<span>Skills Training</span></a></li>
-                  <li><a href="<?php echo home_url(); ?>//">Web講演会<span>Webinars</span></a></li>
-                  <li><a href="<?php echo home_url(); ?>//">情報提供資材<span>Webinars</span></a></li>
-                </ul>
+              <div class="sub-nav sub-nav-sp">
+                <div class="search-link">
+                  <a href="<?php echo home_url(); ?>/plan/">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30.253" height="33.159" viewBox="0 0 30.253 33.159" aria-hidden="true" focusable="false">
+                      <g transform="translate(-1201 -22)">
+                        <path d="M14,2A12,12,0,0,0,5.515,22.485,12,12,0,1,0,22.485,5.515,11.921,11.921,0,0,0,14,2m0-2A14,14,0,1,1,0,14,14,14,0,0,1,14,0Z" transform="translate(1201 22)" fill="currentColor"/>
+                        <path d="M6.247,8.658l-7-8L.753-.659l7,8Z" transform="translate(1223.5 46.5)" fill="currentColor"/>
+                      </g>
+                    </svg>
+                    <span>動画ライブラリ</span>
+                  </a>
+                </div>
+
+                <div class="search-link">
+                  <a href="<?php echo home_url(); ?>/corporation/">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30.253" height="33.159" viewBox="0 0 30.253 33.159" aria-hidden="true" focusable="false">
+                      <g transform="translate(-1201 -22)">
+                        <path d="M14,2A12,12,0,0,0,5.515,22.485,12,12,0,1,0,22.485,5.515,11.921,11.921,0,0,0,14,2m0-2A14,14,0,1,1,0,14,14,14,0,0,1,14,0Z" transform="translate(1201 22)" fill="currentColor"/>
+                        <path d="M6.247,8.658l-7-8L.753-.659l7,8Z" transform="translate(1223.5 46.5)" fill="currentColor"/>
+                      </g>
+                    </svg>
+                    <span>スライド資料</span>
+                  </a>
+                </div>
               </div>
-						</div>
-
-						<div class="sub-nav sub-nav-sp">
-							<div class="search-link">
-								<a href="<?php echo home_url(); ?>/plan/">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="30.253" height="33.159" viewBox="0 0 30.253 33.159" aria-hidden="true" focusable="false">
-                    <g transform="translate(-1201 -22)">
-                      <path d="M14,2A12,12,0,0,0,5.515,22.485,12,12,0,1,0,22.485,5.515,11.921,11.921,0,0,0,14,2m0-2A14,14,0,1,1,0,14,14,14,0,0,1,14,0Z" transform="translate(1201 22)" fill="currentColor"/>
-                      <path d="M6.247,8.658l-7-8L.753-.659l7,8Z" transform="translate(1223.5 46.5)" fill="currentColor"/>
-                    </g>
-                  </svg>
-                  <span>動画ライブラリ</span>
-                </a>
-							</div>
-
-							<div class="search-link">
-								<a href="<?php echo home_url(); ?>/corporation/">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="30.253" height="33.159" viewBox="0 0 30.253 33.159" aria-hidden="true" focusable="false">
-                    <g transform="translate(-1201 -22)">
-                      <path d="M14,2A12,12,0,0,0,5.515,22.485,12,12,0,1,0,22.485,5.515,11.921,11.921,0,0,0,14,2m0-2A14,14,0,1,1,0,14,14,14,0,0,1,14,0Z" transform="translate(1201 22)" fill="currentColor"/>
-                      <path d="M6.247,8.658l-7-8L.753-.659l7,8Z" transform="translate(1223.5 46.5)" fill="currentColor"/>
-                    </g>
-                  </svg>
-                  <span>スライド資料</span>
-                </a>
-							</div>
-						</div>
-					</div>
-					
-					<div class="buttom-nav">
-						<ul>
-							<li class="inquiry"><a href="" target="_blank"><span>お問い合わせ</span></a></li>
-							<li class="download"><a href="<?php echo home_url(); ?>/faq/"><span>ログイン・会員登録</span></a></li>
-						</ul>
-					</div>
-        </div>
-					
-	      </nav>
+            </div>
+            
+            <div class="buttom-nav">
+              <ul>
+                <li class="inquiry"><a href="" target="_blank"><span>お問い合わせ</span></a></li>
+                <li class="download"><a href="<?php echo home_url(); ?>/faq/"><span>ログイン・会員登録</span></a></li>
+              </ul>
+            </div>
+          </div>
+            
+        </nav>
+      </div>
     </div>
   </div>
