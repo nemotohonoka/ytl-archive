3a1d3ef408d26ebc03cef1165df0a9a86ddd8c01
根本萌香 <h.nemoto@p-oh.jp>
2025-08-21 13:56:02 +0900

    会社概要ページ 作成

--- pages/about.php
+++ pages/about.php
@@ -1,11 +1,62 @@
 <?php /*
-Template Name:
+Template Name:会社概要
 */ ?>
 
 
 <?php get_header(); ?>
 
 <main>
+  <section id="library-section-title">
+    <h2>会社概要</h2>
+  </section>
+
+  <div class="container">
+    <table>
+      <tbody>
+        <tr>
+          <th>会社名</th>
+          <td>株式会社 医学アカデミー</td>
+        </tr>
+        <tr>
+          <th>代表取締役</th>
+          <td>穂坂邦夫</td>
+        </tr>
+        <tr>
+          <th>所在地</th>
+          <td>埼玉県川越市中台元町1-18-1</td>
+        </tr>
+        <tr>
+          <th>TEL</th>
+          <td>049-245-6651</td>
+        </tr>
+        <tr>
+          <th>FAX</th>
+          <td>049-246-5027</td>
+        </tr>
+        <tr>
+          <th>設立</th>
+          <td>2023年9月7日</td>
+        </tr>
+        <tr>
+          <th>従業員数</th>
+          <td>280名（出向含む）</td>
+        </tr>
+        <tr>
+          <th>事業内容</th>
+          <td>
+            <ul>
+              <li>予備校の運営</li>
+              <li>薬剤師国家試験に関する講座、講師派遣</li>
+              <li>各種出版物の出版及び販売</li>
+              <li>医療及びヘルスケア関連事業の資材販売、講師派遣</li>
+              <li>医療及びヘルスケア関連事業に関する研修企画、資材作成・販売</li>
+              <li>医療及びヘルスケア関連のコンサルティング事業 など</li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
 </main>
 
