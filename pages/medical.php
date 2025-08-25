64ed1356b022b4002891a3f573835b30b35530b1
根本萌香 <h.nemoto@p-oh.jp>
2025-08-25 17:39:15 +0900

    コンテンツ 詳細ページ 調整

--- pages/medical.php
+++ pages/medical.php
@@ -32,7 +32,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="免疫・膠原病・感染症">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon02.svg" alt="免疫・膠原病・感染症">
             </figure>
             <p>免疫・膠原病・感染症</p>
           </a>
@@ -41,7 +41,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="脳・神経">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon03.svg" alt="脳・神経">
             </figure>
             <p>脳・神経</p>
           </a>
@@ -50,7 +50,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="腎・泌尿器">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon04.svg" alt="腎・泌尿器">
             </figure>
             <p>腎・泌尿器</p>
           </a>
@@ -59,7 +59,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="婦人科・乳腺">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon05.svg" alt="婦人科・乳腺">
             </figure>
             <p>婦人科・乳腺</p>
           </a>
@@ -68,7 +68,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="呼吸器">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon06.svg" alt="呼吸器">
             </figure>
             <p>呼吸器</p>
           </a>
@@ -77,7 +77,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="眼・耳鼻">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon07.svg" alt="眼・耳鼻">
             </figure>
             <p>眼・耳鼻</p>
           </a>
@@ -86,7 +86,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="皮膚">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon08.svg" alt="皮膚">
             </figure>
             <p>皮膚</p>
           </a>
@@ -95,7 +95,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="悪性腫瘍">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon09.svg" alt="悪性腫瘍">
             </figure>
             <p>悪性腫瘍</p>
           </a>
@@ -104,7 +104,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="希少疾患">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon10.svg" alt="希少疾患">
             </figure>
             <p>希少疾患</p>
           </a>
@@ -113,7 +113,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="血液疾患">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon11.svg" alt="血液疾患">
             </figure>
             <p>血液疾患</p>
           </a>
@@ -122,7 +122,7 @@ Template Name: 疾患別コンテンツ 一覧
         <li>
           <a href="">
             <figure>
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon01.svg" alt="統計">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/medical/medical_icon12.svg" alt="統計">
             </figure>
             <p>統計</p>
           </a>
