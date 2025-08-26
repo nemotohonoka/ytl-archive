6081ebe2fd8a0daf5e0c32a1b7de6acf30822606
根本萌香 <h.nemoto@p-oh.jp>
2025-08-26 19:26:20 +0900

    footer スタイル調整

--- footer.php
+++ footer.php
@@ -10,9 +10,11 @@
               <li>
                 <a href="">
                   <div class="flex-box">
-                    <figure>
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/login.svg" alt="YTL">
-                    </figure>
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="34" height="34" viewBox="0 0 34 34">
+                      <g id="グループ_2522" data-name="グループ 2522" transform="translate(-0.257 -0.257)" clip-path="url(#clip-path)">
+                        <path id="パス_10755" data-name="パス 10755" d="M27.127,27.423a8.426,8.426,0,0,0-8.169-6.359H14.745a8.426,8.426,0,0,0-8.169,6.359,14.745,14.745,0,1,1,20.551,0m-1.876,1.54a14.739,14.739,0,0,1-16.805.007,6.32,6.32,0,0,1,6.3-5.8h4.213a6.324,6.324,0,0,1,6.3,5.8Zm-8.4,4.739A16.851,16.851,0,1,0,0,16.851,16.851,16.851,0,0,0,16.851,33.7m0-16.108a3.16,3.16,0,1,1,3.16-3.16,3.16,3.16,0,0,1-3.16,3.16m-5.266-3.16a5.266,5.266,0,1,0,5.266-5.266,5.266,5.266,0,0,0-5.266,5.266" transform="translate(0.235 0.235)" fill="#011976"/>
+                      </g>
+                    </svg>
                     <span>会員登録はこちら</span>
                   </div>
                 </a>
@@ -21,46 +23,17 @@
               <li>
                 <a href="">
                   <div class="flex-box">
-                    <figure>
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/login.svg" alt="YTL">
-                    </figure>
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="34" height="34" viewBox="0 0 34 34">
+                      <g id="グループ_2522" data-name="グループ 2522" transform="translate(-0.257 -0.257)" clip-path="url(#clip-path)">
+                        <path id="パス_10755" data-name="パス 10755" d="M27.127,27.423a8.426,8.426,0,0,0-8.169-6.359H14.745a8.426,8.426,0,0,0-8.169,6.359,14.745,14.745,0,1,1,20.551,0m-1.876,1.54a14.739,14.739,0,0,1-16.805.007,6.32,6.32,0,0,1,6.3-5.8h4.213a6.324,6.324,0,0,1,6.3,5.8Zm-8.4,4.739A16.851,16.851,0,1,0,0,16.851,16.851,16.851,0,0,0,16.851,33.7m0-16.108a3.16,3.16,0,1,1,3.16-3.16,3.16,3.16,0,0,1-3.16,3.16m-5.266-3.16a5.266,5.266,0,1,0,5.266-5.266,5.266,5.266,0,0,0-5.266,5.266" transform="translate(0.235 0.235)" fill="#011976"/>
+                      </g>
+                    </svg>
                     <span>ログインはこちら</span>
                   </div>
                 </a>
               </li>
             </ul>
           </div>
-
-
-          <!-- <div class="contact-button">
-            <ul>
-              <li>
-                <figure>
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/login.svg" alt="YTL">
-                </figure>
-                <p>新規会員登録</p>
-
-                <div class="default-button">
-                  <a href="<?php echo home_url(); ?>/about/" class="button-more"><span>会員登録はこちら</span></a>
-                </div>
-              </li>
-            </ul>
-          </div>
-
-          <div class="contact-button">
-            <ul>
-              <li>
-                <figure>
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/login.svg" alt="YTL">
-                </figure>
-                <p>ログイン</p>
-
-                <div class="default-button">
-                  <a href="<?php echo home_url(); ?>/about/" class="button-more"><span>ログインはこちら</span></a>
-                </div>
-              </li>
-            </ul>
-          </div> -->
         </div>
       </div>
 
