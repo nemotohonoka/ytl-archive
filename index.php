3c9e1da38cf9b530fc9fe40b6bc736cffa7cf8fc
根本萌香 <h.nemoto@p-oh.jp>
2025-09-01 14:01:02 +0900

    お知らせページ 作成

--- index.php
+++ index.php
@@ -1,6 +1,64 @@
 <?php get_header(); ?>
 
 <main>
+  <section id="library-section-title">
+    <h2>お知らせ</h2>
+  </section>
+
+  <div class="container">
+    <?php if(have_posts()): ?>
+  		<ul class="article_list">
+  			<?php while(have_posts()):the_post(); ?>
+  			<?php
+  				$cat = get_the_category();
+  				$cat = $cat[0];
+  			?>
+  			
+  			<li>
+  				<a href="<?php the_permalink(); ?>">
+  					<!-- <figure>
+  						<?php if (has_post_thumbnail()) : ?>
+  							<?php echo get_thumb_img('thumbnail'); ?>
+  						<?php else : ?>
+  							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/no_image.jpg" alt="アスベスト患者と家族の会 連絡会">
+  						<?php endif ; ?>	              
+  					</figure> -->
+  					<section>
+  						<div class="label <?php echo $cat->category_nicename; ?>"><?php echo $cat->cat_name; ?></div>
+  						<?php if (in_category('system')):?>
+  							<span class="sub-category">
+  								<?php $cats = get_the_category();
+  								foreach($cats as $cat):
+  								if($cat->parent)
+  								echo $cat->cat_name;
+  								endforeach;
+  								?>
+  							</span>
+  						<?php endif; ?>
+  						<?php if (in_category('medical')):?>
+  							<span class="sub-category">
+  								<?php $cats = get_the_category();
+  								foreach($cats as $cat):
+  								if($cat->parent)
+  								echo $cat->cat_name;
+  								endforeach;
+  								?>
+  							</span>
+  						<?php endif; ?>
+  
+  						<time><?php the_time("Y.m.d"); ?></time>
+  						<h3><?php the_title(); ?></h3>
+  						<?php the_excerpt(); ?>
+  						<span class="readmore">続きを読む</span>
+  					</section>
+  				</a>
+  			</li>
+  			<?php endwhile; ?>
+  		</ul>
+    <?php endif; ?>
+  		
+    <?php do_shortcode('[pagination]') ?>
+  </div>
 
 </main>
 	  
--- single.php
+++ single.php
@@ -1,6 +1,85 @@
 <?php get_header(); ?>
 
 <main>	
+  <section id="library-section-title">
+    <h2>お知らせ</h2>
+  </section>
+
+<div class="container">
+		
+		
+		<?php if(have_posts()): ?>
+		<ul class="breadcrumb">
+			<li><a href="<?php echo home_url(); ?>/news/">ニュース一覧</a></li>
+			<li class="title"><?php the_title(); ?></li>
+		</ul>
+		
+	  <?php while(have_posts()):the_post(); ?>
+		<?php
+			$cat = get_the_category();
+			$cat = $cat[0];
+		?>
+		
+		<div class="flexbox">
+			
+			
+			
+			<article>
+				<div class="top">
+					<div class="information">
+						<time><?php the_time("Y年n月j日"); ?></time>
+						<span class="label <?php echo $cat->category_nicename; ?>"><?php echo $cat->cat_name; ?></span>
+					</div>
+					<h3><?php the_title(); ?></h3>
+				</div>
+				<div class="contents">
+					<?php the_content(); ?>
+				</div>
+	
+				<ul class="button_area">
+  					<?php if (get_previous_post()):?>
+  						<li class="prev"><?php previous_post_link('%link', '前の記事'); ?></li>
+  					<?php endif; ?>
+  
+  
+  					<?php if (get_next_post()):?>
+  						<li class="next"><?php next_post_link('%link', '次の記事'); ?></li>
+  					<?php endif; ?>
+  
+  				</ul>
+			</article>
+		  <?php endwhile;endif; ?>
+
+			<div class="sideber">
+				<?php
+					$the_query = new WP_Query(
+						array(
+							'post_type' => 'post',
+							'posts_per_page' => 5
+						)
+					);
+				?>
+				<?php if($the_query->have_posts()): ?>
+				<!-- <section class="latest">
+					<h3>最近のニュース</h3>
+					<ul class="article_list">
+						<?php while($the_query->have_posts()):$the_query->the_post(); ?>
+						<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+						<?php endwhile; ?>
+					</ul>
+				</section> -->
+				<?php endif; ?>
+				<?php wp_reset_postdata(); ?>
+				
+				<section class="archive">
+					<h3>ARCHIVE</h3>
+					<ul>
+						<?php wp_get_archives(); ?>
+					</ul>
+				</section>
+			</div>			
+		</div>
+	</div>
 
 </main>
 
