59700c8c41eefe1b46e80b6a8846e9f16d87a6d8
根本萌香 <h.nemoto@p-oh.jp>
2025-11-20 11:07:22 +0900

    投稿スラッグ 変更

--- pages/medical_metabolic.php
+++ pages/medical_metabolic.php
@@ -27,7 +27,7 @@ Template Name: 代謝・内分泌
     <div class="posts">
       <?php
       // child01 のタームを取得
-      $child_term = get_term_by('slug', 'child11-parent01', 'common_category');
+      $child_term = get_term_by('slug', 'child14-parent01', 'common_category');
 
       if ($child_term) {
         $query = new WP_Query([
@@ -145,7 +145,7 @@ Template Name: 代謝・内分泌
     </div>
 
     <div class="default-button">
-      <button class="button-more library-more" data-parent="parent01" data-child="child11-parent01">
+      <button class="button-more library-more" data-parent="parent01" data-child="child14-parent01">
         <span>もっと見る</span>
       </button>
     </div>
@@ -160,7 +160,7 @@ Template Name: 代謝・内分泌
     <div class="posts">
       <?php
       // child01 のタームを取得
-      $child_term = get_term_by('slug', 'child11-parent01', 'common_category');
+      $child_term = get_term_by('slug', 'child14-parent01', 'common_category');
 
       if ($child_term) {
         $query = new WP_Query([
@@ -278,7 +278,7 @@ Template Name: 代謝・内分泌
     </div>
 
     <div class="default-button">
-      <button class="button-more library-more" data-parent="parent01" data-child="child11-parent01">
+      <button class="button-more library-more" data-parent="parent01" data-child="child14-parent01">
         <span>もっと見る</span>
       </button>
     </div>
